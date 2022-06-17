604e6481b49894ffab17b12f6ec06dbf5026f4f1
alexsmaldone <alexsmaldone@gmail.com>
2022-06-17 18:44:48 -0400

    added transaction model from pydantic

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from sqlite3 import Timestamp
 from fastapi import FastAPI
+from pydantic import BaseModel
 from datetime import datetime
 
 app = FastAPI()
+
+class Transaction(BaseModel):
+  payer: str
+  points: int
+  timestamp: datetime
 
 user_points = 0
 payer_points = {}
@@ -12,10 +19,10 @@ transactions = []
 def get_payer_points():
     return payer_points
 
-@app.post("/")
+@app.post("/transactions")
 def add_transaction():
   pass
 
-@app.post("/spend")
+@app.post("/spending")
 def spend_payer_points():
   pass
