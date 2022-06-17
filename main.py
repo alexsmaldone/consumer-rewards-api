e1dbb0e082e8f1c3515056d9ce5efe004f397d0b
alexsmaldone <alexsmaldone@gmail.com>
2022-06-17 18:56:08 -0400

    added user spend model

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ from datetime import datetime
 
 app = FastAPI()
 
-class Transaction(BaseModel):
+class PayerTransaction(BaseModel):
   payer: str
   points: int
   timestamp: datetime
 
-user_points = 0
+class UserSpend(BaseModel):
+  points: int
+
+total_points = 0
 payer_points = {}
 transactions = []
 
 
-@app.get("/")
+@app.get("/point_balance")
 def get_payer_points():
     return payer_points
 
-@app.post("/transactions")
-def add_transaction():
+@app.post("/payer_transactions")
+def add_transaction(transaction: PayerTransaction):
   pass
 
 @app.post("/spending")
-def spend_payer_points():
+def spend_payer_points(spend: UserSpend):
   pass
